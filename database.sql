6c193ad6ef4053c4ccc762d72dbf3ef58c3ca647
Velsych <velsych@mail.ru>
2025-11-10 17:15:08 +0300

    added post check fix #1

--- database.sql
+++ database.sql
@@ -1,4 +1,3 @@
-DROP TABLE urls;
 CREATE TABLE IF NOT EXISTS urls(
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
@@ -6,7 +5,7 @@ CREATE TABLE IF NOT EXISTS urls(
 );
 CREATE TABLE IF NOT EXISTS url_checks(
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    url_id REFERENCES urls(id),
+    url_id BIGINT REFERENCES urls(id),
     status_code TINYINT,
     h1 VARCHAR(255),
     title VARCHAR(255),
